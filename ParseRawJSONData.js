96840de2d7648df1620f166454993db3266250a2
Groove Theory <lawar723@att.net>
2020-11-07 23:01:27 -0600

    Update ParseRawJSONData.js

--- ParseRawJSONData.js
+++ ParseRawJSONData.js
@@ -1,4 +1,4 @@
-var aPoses = [];
+var g_aPoses = [];
 
 class YogaPose {
     static from(json){
@@ -7,24 +7,32 @@ class YogaPose {
 }
 
 
-keys = Object.keys(localStorage),
-i = keys.length;
 
-while ( i-- ) {
-    let data = JSON.parse(localStorage.getItem(keys[i]));
-    aPoses.push(YogaPose.from(data))
+function parseRawJSONData(oRawJson)
+{
+    keys = Object.keys(oRawJson),
+    i = keys.length;
+
+    while ( i-- ) {
+        let data = JSON.parse(oRawJson.getItem(keys[i]));
+        g_aPoses.push(YogaPose.from(data))
+    }
+
+    g_aPoses.forEach(pose => {
+        let aVariationNames = pose.Variations;
+        let aVariationMap = g_aPoses.filter(p => aVariationNames.indexOf(p.EnglishName) > -1);
+        pose.Variations = aVariationMap;
+
+        let aPreviousPosesNames = pose.PreviousPoses;
+        let aPreviousPosesMap = g_aPoses.filter(p => aPreviousPosesNames.indexOf(p.EnglishName) > -1);
+        pose.PreviousPoses = aPreviousPosesMap;
+
+        let aNextPosesNames = pose.NextPoses;
+        let aNextPosesMap = g_aPoses.filter(p => aNextPosesNames.indexOf(p.EnglishName) > -1);
+        pose.NextPoses = aNextPosesMap;
+    })
 }
 
-aPoses.forEach(pose => {
-    let aVariationNames = pose.Variations;
-    let aVariationMap = aPoses.filter(p => aVariationNames.indexOf(p.EnglishName) > -1);
-    pose.Variations = aVariationMap;
-
-    let aPreviousPosesNames = pose.PreviousPoses;
-    let aPreviousPosesMap = aPoses.filter(p => aPreviousPosesNames.indexOf(p.EnglishName) > -1);
-    pose.PreviousPoses = aPreviousPosesMap;
-
-    let aNextPosesNames = pose.NextPoses;
-    let aNextPosesMap = aPoses.filter(p => aNextPosesNames.indexOf(p.EnglishName) > -1);
-    pose.NextPoses = aNextPosesMap;
-})
+window.onload = $.getJSON("raw_data.json", function(json) {
+    parseRawJSONData(json);
+});;
